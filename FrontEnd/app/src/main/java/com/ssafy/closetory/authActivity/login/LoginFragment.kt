c6465f8a76ee9e53cee79ae45a7f2902384212ff
JinYunSe <wlsdbstp1234@gmail.com>
2026-01-16 17:35:01 +0900

    Init(front) : 회원 가입 Fragment 추가

---
+++ FrontEnd/app/src/main/java/com/ssafy/closetory/authActivity/login/LoginFragment.kt
@@ -0,0 +1,39 @@
+package com.ssafy.closetory.authActivity.login
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.ssafy.closetory.R
+import com.ssafy.closetory.authActivity.AuthActivity
+import com.ssafy.closetory.databinding.FragmentClosetBinding
+import com.ssafy.closetory.databinding.FragmentHomeBinding
+import com.ssafy.closetory.databinding.FragmentLoginBinding
+import com.ssafy.closetory.homeActivity.HomeActivity
+import com.ssafy.ssafyfinalproject.baseCode.base.BaseActivity
+import com.ssafy.ssafyfinalproject.baseCode.base.BaseFragment
+
+private const val TAG = "LoginFragment_싸피"
+class LoginFragment : BaseFragment<FragmentLoginBinding>(FragmentLoginBinding::bind, R.layout.fragment_login) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnSignUp.setOnClickListener {
+            Log.d(TAG, "SignUp 이동 버튼 : 동작 유무 확인")
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.auth_container, SignUpFragment())
+                .addToBackStack(null)
+                .commit()
+        }
+
+        binding.btnLogin.setOnClickListener {
+            Log.d(TAG, "화면 전환 버튼 동작 유무 확인")
+            startActivity(Intent(requireContext(), HomeActivity::class.java))
+        }
+    }
+}
