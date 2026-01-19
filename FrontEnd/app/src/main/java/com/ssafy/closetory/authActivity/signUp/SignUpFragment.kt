b4bf7dc68a9b8d69855f473264e4c9d140e18ebf
Choi Inho <choiinho97@naver.com>
2026-01-19 17:58:59 +0900

    ë회원가입 UI S14P11D102-196)

--- FrontEnd/app/src/main/java/com/ssafy/closetory/authActivity/signUp/SignUpFragment.kt
+++ FrontEnd/app/src/main/java/com/ssafy/closetory/authActivity/signUp/SignUpFragment.kt
@@ -1,5 +1,6 @@
 package com.ssafy.closetory.authActivity.signUp
 
+import android.content.res.ColorStateList
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -11,15 +12,51 @@ import com.ssafy.ssafyfinalproject.baseCode.base.BaseFragment
 private const val TAG = "SignUpFragment_싸피"
 class SignUpFragment : BaseFragment<FragmentSignUpBinding>(FragmentSignUpBinding::bind, R.layout.fragment_sign_up) {
 
+    private var selectedGender: String? = null // 남,여 선택 버튼 토글화 하기 위함.
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.btnLogin.setOnClickListener {
+        setupGenderButtons()
+
+        binding.tvYesIdToLogin.setOnClickListener {
             Log.d(TAG, "Login 버튼 : 동작 유무 확인")
             parentFragmentManager.beginTransaction()
                 .replace(R.id.auth_container, LoginFragment())
                 .addToBackStack(null)
                 .commit()
+        }
+    }
+
+    private fun setupGenderButtons() {
+        binding.btnFemale.setOnClickListener {
+            selectGender("FEMALE")
+        }
+
+        binding.btnMale.setOnClickListener {
+            selectGender("MALE")
+        }
+    }
+
+    private fun selectGender(gender: String) {
+        selectedGender = gender
+
+        val gray = requireContext().getColor(R.color.gray_500)
+        val sky = requireContext().getColor(R.color.main_color)
+
+        binding.btnFemale.backgroundTintList =
+            ColorStateList.valueOf(gray)
+        binding.btnMale.backgroundTintList =
+            ColorStateList.valueOf(gray)
+
+        when (gender) {
+            "FEMALE" ->
+                binding.btnFemale.backgroundTintList =
+                    ColorStateList.valueOf(sky)
+
+            "MALE" ->
+                binding.btnMale.backgroundTintList =
+                    ColorStateList.valueOf(sky)
         }
     }
 }
