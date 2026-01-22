c963b0c1bcc92224056be9f3681e031588877f37
Choi Inho <choiinho97@naver.com>
2026-01-22 22:54:26 +0900

    Feat(front) : 로그아웃 서버통신 기능 구현

--- FrontEnd/app/src/main/java/com/ssafy/closetory/authActivity/logout/LogoutRepository.kt
+++ FrontEnd/app/src/main/java/com/ssafy/closetory/authActivity/logout/LogoutRepository.kt
@@ -1,4 +1,13 @@
+// LogoutService
+
 package com.ssafy.closetory.authActivity.logout
 
+import com.ssafy.closetory.ApplicationClass
+
 class LogoutRepository {
+
+    private val service =
+        ApplicationClass.retrofit.create(LogoutService::class.java)
+
+    suspend fun logout(accessToken: String) = service.logout(accessToken)
 }
--- FrontEnd/app/src/main/java/com/ssafy/closetory/authActivity/logout/LogoutService.kt
+++ FrontEnd/app/src/main/java/com/ssafy/closetory/authActivity/logout/LogoutService.kt
@@ -1,4 +1,13 @@
+// LogoutService
+
 package com.ssafy.closetory.authActivity.logout
 
-class LogoutService {
+import retrofit2.Response
+import retrofit2.http.Header
+import retrofit2.http.POST
+
+interface LogoutService {
+
+    @POST("auth/logout")
+    suspend fun logout(@Header("Authorization") accessToken: String): Response<Unit>
 }
