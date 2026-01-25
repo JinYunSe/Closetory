5bb67a6d7d0ac5aa804d22d4ad32568dde4a59e5
Choi Inho <choiinho97@naver.com>
2026-01-25 16:43:37 +0900

    Feat(front) : 회원정보 수정 서버통신 코드 수정 및 UserId 조회 방법 변경 (S14P11D102-199 / S14P11D102-224)

--- FrontEnd/app/src/main/java/com/ssafy/closetory/dto/EditProfileBaseResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.ssafy.closetory.dto
-
-// 비밀번호 변경 Response DTO
-
-data class EditProfileBaseResponse(
-    val httpStatusCode: Int,
-    val responseMessage: String?, // 성공
-    val errorMessage: String? // 실패
-)
