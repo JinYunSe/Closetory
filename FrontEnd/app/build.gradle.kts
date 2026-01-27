49a9e5d3c73609b5abc0e27328c4611c10dc8925
진윤세 <dbstp1234@naver.com>
2026-01-27 16:08:03 +0900

    Chore(front): glide 과 관련해 추가적인 라이브러리 추가함

--- FrontEnd/app/build.gradle.kts
+++ FrontEnd/app/build.gradle.kts
@@ -119,6 +119,7 @@ dependencies {
     implementation("com.google.android.material:material:1.12.0")
     implementation("com.google.android.flexbox:flexbox:3.0.0")
     implementation("com.github.bumptech.glide:glide:4.16.0")
+    kapt("com.github.bumptech.glide:compiler:4.16.0")
 
     // Coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
