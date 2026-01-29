05d32fc390c739e22314ae62fee9da503238245b
Choi Inho <choiinho97@naver.com>
2026-01-29 20:05:58 +0900

    Refactor(front) : 불필요한 코드 제거 (S14P11D102-199)

--- FrontEnd/app/src/main/java/com/ssafy/closetory/dto/EditProfileInfoResponse.kt
+++ FrontEnd/app/src/main/java/com/ssafy/closetory/dto/EditProfileInfoResponse.kt
@@ -1,15 +1,5 @@
 package com.ssafy.closetory.dto
 
-// 요청 형태
-// data class EditProfileInfoResponse(
-//    val nickname: String,
-//    val gender: String, // FEMALE / MALE
-//    val height: Int,
-//    val weight: Int,
-//    val alarmEnabled: Boolean,
-//    val profilePhotoUrl: String?, // 프로필 사진
-//    val bodyPhotoUrl: String? // 전신 사진
-// )
 data class EditProfileInfoResponse(
     val nickname: String,
     val gender: String, // FEMALE / MALE
