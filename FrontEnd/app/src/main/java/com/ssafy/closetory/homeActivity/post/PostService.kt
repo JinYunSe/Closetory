3f78335c4f28d65b22ef7c2074a90ccda9432ee2
Choi Inho <choiinho97@naver.com>
2026-01-29 10:06:08 +0900

    Freat(front) : 게시글 목록 조회 기능 구현 (S14P11D102-287)

--- FrontEnd/app/src/main/java/com/ssafy/closetory/homeActivity/post/PostService.kt
+++ FrontEnd/app/src/main/java/com/ssafy/closetory/homeActivity/post/PostService.kt
@@ -1,21 +1,21 @@
 package com.ssafy.closetory.homeActivity.post
-//
-// import com.ssafy.closetory.dto.ApiResponse
-// import com.ssafy.closetory.dto.PostItemResponse
-// import com.ssafy.closetory.dto.PostQueryFilter
-// import retrofit2.Response
-// import retrofit2.http.GET
-// import retrofit2.http.Query
-//
+
+import com.ssafy.closetory.dto.ApiResponse
+import com.ssafy.closetory.dto.PostItemResponse
+import com.ssafy.closetory.dto.PostQueryFilter
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 // 게시판 관련 API 정의
 interface PostService {
-//
-//    // 게시글 목록/검색 조회
-//    // keyword: 검색어 (null이면 전체)
-//    // filter: liked / written / latest / popular (라디오에서 1개 선택)
-//    @GET("posts")
-//    suspend fun getPosts(
-//        @Query("keyword") keyword: String? = null,
-//        @Query("filter") filter: PostQueryFilter? = null
-//    ): Response<ApiResponse<List<PostItemResponse>>>
+
+    // 게시글 목록/검색 조회
+    // keyword: 검색어 (null이면 전체)
+    // filter: liked / written / latest / popular (라디오에서 1개 선택)
+    @GET("posts")
+    suspend fun getPosts(
+        @Query("keyword") keyword: String? = null,
+        @Query("filter") filter: PostQueryFilter? = null
+    ): Response<ApiResponse<List<PostItemResponse>>>
 }
